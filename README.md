e628160d227cbde22176c09edb5bfeddcc57153f
xrr2016 <13286904669@163.com>
2017-02-08 20:10:47 +0800

    :tada: start

---
+++ README.md
@@ -0,0 +1,11 @@
+## 微信小程序 简易版V2EX社区
+
+
+
+
+- written by xrr2016
+
+
+## License
+
+   MIT	
---
+++ app.js
@@ -0,0 +1,12 @@
+//app.js
+App({
+  onLaunch: function () {
+  
+  },
+  getUserInfo:function(){
+ 
+  },
+  globalData:{
+    
+  }
+})
