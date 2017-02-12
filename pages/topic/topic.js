87c04ed61db5b2b6e65941541066a4170f574359
xrr2016 <13286904669@163.com>
2017-02-12 21:37:04 +0800

    :zap: edit topic page

--- pages/topic/topic.js
+++ pages/topic/topic.js
@@ -1,12 +1,35 @@
 // pages/topic/topic.js
+import { topic,topicReplies } from "../../utils/API"
 Page({
   data:{
-    topicId : ""
+    topicId :null,
+    topic : {},
+    replies :[]
   },
   onLoad:function(options){
     const self = this
     this.setData({
       topicId : options.id
+    }),
+    wx.request({
+      url: `${ topic }?id=${self.data.topicId}`,
+      method: 'GET',
+      success: function(res){
+        self.setData({
+          topic : res.data[0]
+        })
+        console.dir(self.data.topic)
+      }
+    })
+    wx.request({
+      url: `${topicReplies}?topic_id=${self.data.topicId}`,
+      method: 'GET',
+      success: function(res){
+        self.setData({
+          replies : res.data
+        })
+        console.dir(self.data.replies)
+      }
     })
   }
 })
--- utils/API.js
+++ utils/API.js
@@ -3,8 +3,9 @@ const latestTopics = baseURL + "/topics/latest.json"
 const hotTopics = baseURL + "/topics/hot.json"
 const userinfo = baseURL + "/members/show.json"
 const nodeinfo = baseURL + "/nodes/show.json"
+const topic = baseURL + "/topics/show.json"
 const topicReplies = baseURL + "/replies/show.json"
 const siteinfo = baseURL + "/site/info.json"
 const stateinfo = baseURL + "/site/stats.json"
 
-export {baseURL,latestTopics,hotTopics,userinfo,nodeinfo,topicReplies,siteinfo,stateinfo}
+export {baseURL,latestTopics,hotTopics,userinfo,nodeinfo,topic,topicReplies,siteinfo,stateinfo}
